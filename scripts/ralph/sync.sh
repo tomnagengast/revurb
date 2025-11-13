a496e1948df3e361b2b4caf68547773c4888a864
tomnagengast <tnagengast@gmail.com>
2025-11-13 12:27:02 -0800

    review: update status for 1fddd18
    Signed-off-by: tomnagengast <tnagengast@gmail.com>

--- scripts/ralph/sync.sh
+++ scripts/ralph/sync.sh
@@ -3,16 +3,31 @@ set -eo pipefail
 
 prompt="$(<scripts/ralph/prompt-fast.md)"
 out="specs/logs/codex-$(date +%Y-%m-%d-%H%M).md"
+
+function cursor() {
+    # Thanks https://github.com/johnlindquist/cursor-alias
+    A_MARKDOWN=1 A_LOG_FILE="specs/logs/composer-$(date +%Y-%m-%d-%H%M).md" a --force "$1"
+}
+
+function codex() {
+    cat scripts/ralph/prompt-smart.md | \
+        codex --yolo exec --skip-git-repo-check \
+        --output-last-message "$1"
+}
+function claude() {
+    printf "%s" "$1" | \
+        claude -p --output-format=stream-json --verbose --dangerously-skip-permissions | \
+        tee -a _tmp/ralph/claude_output.jsonl | \
+        npx repomirror visualize --debug
+}
+
+
 git pull origin main || { git stash && git pull origin main && git stash pop; }
 
 bun run .claude/hooks/discord.ts --start --message "$prompt"
 
-# Thanks https://github.com/johnlindquist/cursor-alias
-A_MARKDOWN=1 A_LOG_FILE="specs/logs/composer-$(date +%Y-%m-%d-%H%M).md" a --force "$prompt"
-
-cat scripts/ralph/prompt-smart.md | \
-    codex --yolo exec --skip-git-repo-check \
-    --model gpt-5-codex \
-    --output-last-message $out
+cursor "$prompt"
+# claude "$prompt"
+codex "$out"
 
 bun run .claude/hooks/discord.ts --stop --message "$(<$out)"
