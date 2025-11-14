85e56113cc6629bbbfef3315f0f385cbbca3c3aa
tomnagengast <tnagengast@gmail.com>
2025-11-13 21:16:57 -0800

    app: update copy

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,25 +5,24 @@ import githubLogo from "./github.svg";
 export function App() {
   return (
     <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
-      <h1 className="text-5xl font-bold my-4 leading-tight">
-        Revurb Chat Example
-      </h1>
+      <h1 className="text-5xl font-bold my-4 leading-tight">Revurb</h1>
       <a
         href="https://github.com/tomnagengast/revurb"
         target="_blank"
         rel="noopener noreferrer"
       >
-        {/* this should be white */}
         <img
           src={githubLogo}
           alt="GitHub"
-          className="w-10 h-10 inline-block mb-4"
+          className="w-6 h-6 inline-block mb-4 invert"
         />
       </a>
-      <p className="mb-4">
-        A real-time chat demo using Revurb WebSocket server. Enter your name,
-        connect, join a channel, and start messaging. Open multiple browser tabs
-        to chat with yourself!
+      <p className="mb-4 max-w-2xl">
+        A real-time chat demo using Revurb WebSocket server.
+        <br />
+        Enter your name, connect, join a channel, and start messaging.
+        <br />
+        Open multiple browser tabs to chat with yourself!
       </p>
       <Chat />
     </div>
